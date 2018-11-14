be5e976f7eb5facb4a1e6fec7e31e04edde07b6c
jaab30 <jaab30@yahoo.com>
2018-11-14 14:48:16 -0500

    first final commit

--- data/friend.js
+++ data/friend.js
@@ -1,19 +1,45 @@
 
 
 var friendArray = [{
-    "name": "Ahmed",
-    "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "name": "Ellen",
+    "photo": "https://cdn2.stylecraze.com/wp-content/uploads/2013/01/2162_Eyebrows-For-Square-Face-Shape_shutterstock_148055564.jpg",
     "scores": [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
 },
 {
     "name": "John",
-    "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "photo": "https://floridacommunity.com/wp-content/uploads/2015/10/Brian-Tietz-e1446241114449.jpg",
     "scores": [3, 3, 5, 1, 3, 2, 3, 5, 3, 2]
 },
 {
-    "name": "chris",
-    "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "name": "Chris",
+    "photo": "https://www.tugraz.at/fileadmin/_migrated/pics/BTF_2017_Benedict_by_Kanizaj-tugraz.jpg",
     "scores": [1, 2, 5, 3, 3, 1, 4, 2, 2, 3]
+},
+{
+    "name": "Nina",
+    "photo": "https://cdnb.artstation.com/p/assets/images/images/001/863/575/large/irakli-nadar-artstation-da.jpg?1453903033",
+    "scores": [1, 3, 3, 4, 5, 2, 4, 1, 5, 3]
+
+},
+{
+    "name": "Jessica",
+    "photo": "https://media.glamour.com/photos/5a425fd3b6bcee68da9f86f8/master/w_644,c_limit/best-face-oil.png",
+    "scores": [2, 3, 3, 2, 5, 1, 3, 4, 2, 3]
+},
+{
+    "name": "Jim",
+    "photo": "http://d3cdsjlahqfkbd.cloudfront.net/13067/photo_1510911006.jpg",
+    "scores": [5, 5, 1, 3, 5, 1, 2, 4, 1, 5]
+},
+{
+    "name": "Denise",
+    "photo": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSDZCBHvBdp94OrTGqiNXjGQLMQA7K9FTQN4t2VQ11RcrrztDutIA",
+    "scores": [4, 2, 5, 2, 1, 4, 1, 2, 3, 4]
+},
+{
+    "name": "Joseph",
+    "photo": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRkuiZsOfsXBeHvVo1BHQuIyU6Co1UEtvxO9xyaD7GfG36pin5blA",
+    "scores": [3, 4, 3, 5, 1, 5, 5, 2, 2, 3]
 }
 ];
 
